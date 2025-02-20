3e611a1995c82f1a1b9e8de07f7ed51231b4463c
asdeveloop <asdevelooper@gmail.com>
2025-02-20 10:49:42 +0330

    Add post listing, detail, like, and comment endpoints; update URL routing and schema documentation

--- backend/api/views.py
+++ backend/api/views.py
@@ -67,5 +67,90 @@ class PostCategoryListAPIView(generics.ListAPIView):
         return api_models.Post.objects.filter(category=category, status='Active')
     
 
+class PostListAPIView(generics.ListAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = api_serializer.PostSerializer
+
+    def get_queryset(self):
+        return api_models.Post.objects.filter(status='Active')
         
 
+class PostDetailAPIView(generics.RetrieveAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = api_serializer.PostSerializer
+
+    def get_object(self):
+        slug = self.kwargs['slug']
+        post = api_models.Post.objects.get(slug=slug, status="Active")
+        post.view += 1
+        post.save()
+        return post
+
+
+class LikePostAPIView(APIView):
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'user_id': openapi.Schema(type=openapi.TYPE_INTEGER),
+                'post_id': openapi.Schema(type=openapi.TYPE_INTEGER),
+            },
+        ),
+    )
+
+    def post(self, request):
+        user_is = request.data['user_id']
+        post_id = request.data['post_id']
+
+        user = api_models.User.objects.get(id=user_is)
+        post = api_models.Post.objects.get(id=post_id)
+
+        if user in post.likes.all():
+            post.likes.remove(user)
+            return Response({"message": "Post Disliked"}, status=status.HTTP_200_OK)
+        else:
+            post.likes.add(user)
+
+            api_models.Notification.objects.create(
+                user=post.user,
+                post=post,
+                type="Like"
+            )
+            return Response({"message": "Post Liked"}, status=status.HTTP_201_CREATED)
+        
+
+class PostCommentAPIView(APIView):
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'post_id': openapi.Schema(type=openapi.TYPE_INTEGER),
+                'name': openapi.Schema(type=openapi.TYPE_STRING),
+                'email': openapi.Schema(type=openapi.TYPE_STRING),
+                'comment': openapi.Schema(type=openapi.TYPE_STRING),
+            },)
+    )
+    
+    def post(self, request):
+        post_id = request.data['post_id']
+        name = request.data['name']
+        email = request.data['email']
+        comment = request.data['comment']
+
+        post = api_models.Post.objects.get(id=post_id)
+
+        api_models.Comment.objects.create(
+            post=post,
+            name=name,
+            email=email,
+            comment=comment
+        )
+
+        api_models.Notification.objects.create(
+            user=post.user,
+            post=post,
+            type="Comment"
+        )
+
+        return Response({"message": "Comment Added"}, status=status.HTTP_201_CREATED)
+        
